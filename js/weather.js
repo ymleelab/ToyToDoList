cef50e5b7892db56fda6308031f5a73059f0591b
ymleelab <ymleelab@gmail.com>
2022-02-21 01:30:43 +0900

    Nomadcoders Vanillajs Challenge Final

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+const API_KEY = "6cb3d839bdc850599b7b2c5d624100b5";
+
+function onGeoOk(position) {
+    console.log(position);
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    console.log(url);
+    fetch(url).then(response => response.json()).then(data => {
+        //console.log(data.name, data.weather[0].main);
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+function onGeoError() {
+    alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
